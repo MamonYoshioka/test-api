a066bd0940a128ecbdae6433e5e7273983ecb696
Mon <yoshiokamamon@gmail.com>
2026-05-01 01:32:07 +0900

    APIデータの追加

---
+++ data/recipes.js
@@ -0,0 +1,16 @@
+const recipes = [
+    {
+        id: 1,
+        title: "カレー",
+        ingredients: ["にんじん", "じゃがいも"],
+        image: "curry.jpg"
+    },
+    {
+        id: 2,
+        title: "オムライス",
+        ingredients: ["卵", "ごはん"],
+        image: "omelette.jpg"
+    },
+]
+
+export default recipes;
--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 import express from "express";
+import recipes from "./data/recipes.js"
 
 const app = express();
 app.use(express.json());
 
 
-// 動作確認
-app.get("/", (req, res)=>{
-  res.json({message: "Running API"})
+//secure
+const checkAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token !== process.env.ADMIN_TOKEN) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  next();
+};
+
+// 一覧API取得
+app.get("/recipes", (req,res)=>{
+  res.json(recipes);
+})
+
+// 詳細API取得
+app.get("/recipes/:id", (req,res)=>{
+  const id = Number(req.params.id);
+  const recipe = recipes.find(r=>r.id === id);
+
+  if(!recipe){
+    return res.status(404).json({message: "Not Found ID"});
+  }
+
+  res.json(recipe);
 });
 
-// サンプルAPI
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello API" });
+// データ追加
+app.post("/recipes", checkAuth, (req, res) => {
+  const newRecipe = req.body;
+
+  recipes.push(newRecipe);
+
+  res.status(201).json(newRecipe);
 });
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
